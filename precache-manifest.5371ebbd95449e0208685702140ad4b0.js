8bd46cad9ba6f4e1536ee866293e1833bbae8f7a
Gabriel Angel Pinto <gpinto.personal@gmail.com>
2020-02-19 00:56:15 -0400

    s

---
+++ precache-manifest.5371ebbd95449e0208685702140ad4b0.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5933e60b39c423939128",
+    "url": "css/2.fff605f9.css"
+  },
+  {
+    "revision": "86da3cf45d0ae39957df",
+    "url": "css/app.a7120177.css"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "fonts/KFOkCnqEu92Fr1MmgVxIIzQ.5cb7edfc.woff"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "fonts/KFOlCnqEu92Fr1MmEU9fBBc-.87284894.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "fonts/KFOlCnqEu92Fr1MmSU5fBBc-.b00849e0.woff"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "fonts/KFOlCnqEu92Fr1MmWUlfBBc-.adcde98f.woff"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "fonts/KFOlCnqEu92Fr1MmYUtfBBc-.bb1e4dc6.woff"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "fonts/KFOmCnqEu92Fr1Mu4mxM.60fa3c06.woff"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.29b882f0.woff"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.0509ab09.woff2"
+  },
+  {
+    "revision": "76fbf2d99ec1cfc0fab5ecdcdfae81ba",
+    "url": "index.html"
+  },
+  {
+    "revision": "5933e60b39c423939128",
+    "url": "js/2.5933e60b.js"
+  },
+  {
+    "revision": "c29ac579de00e34cb8e8",
+    "url": "js/3.c29ac579.js"
+  },
+  {
+    "revision": "039ea1fe44351ed384c0",
+    "url": "js/4.039ea1fe.js"
+  },
+  {
+    "revision": "5e4119059f7bc18a7b14",
+    "url": "js/5.5e411905.js"
+  },
+  {
+    "revision": "6156d95005d8ff6f1537",
+    "url": "js/6.6156d950.js"
+  },
+  {
+    "revision": "86da3cf45d0ae39957df",
+    "url": "js/app.81105efa.js"
+  },
+  {
+    "revision": "bde75071ec375ebe2f0d",
+    "url": "js/vendor.bde75071.js"
+  },
+  {
+    "revision": "a28e442a406c8f8478f465ac43d72809",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "805b0a66d63db95a76ddda23e3a8c12f",
+    "url": "statics/app-logo-128x128.png"
+  },
+  {
+    "revision": "bde75c95c26cbe407d8c3a51149c9047",
+    "url": "statics/icons/apple-icon-120x120.png"
+  },
+  {
+    "revision": "1e6959a9778d40656217fe5eb7e0b7aa",
+    "url": "statics/icons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "e776afac4479ee2ad8d6a0397661bf09",
+    "url": "statics/icons/apple-icon-167x167.png"
+  },
+  {
+    "revision": "37ab9045dbdc0edf12ea59b3772e19f9",
+    "url": "statics/icons/apple-icon-180x180.png"
+  },
+  {
+    "revision": "65d8571ec508d854ae4132e6540c476a",
+    "url": "statics/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "7233d85b329c161211830b1b30fdf0ec",
+    "url": "statics/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "c7428f8eba0499407710120fea46ead7",
+    "url": "statics/icons/favicon-96x96.png"
+  },
+  {
+    "revision": "04671eaea0c2fc94e31c5ab76ec67d08",
+    "url": "statics/icons/favicon.ico"
+  },
+  {
+    "revision": "5524bdabc6c41e46672771f480cd3d25",
+    "url": "statics/icons/icon-128x128.png"
+  },
+  {
+    "revision": "5524bdabc6c41e46672771f480cd3d25",
+    "url": "statics/icons/icon-192x192.png"
+  },
+  {
+    "revision": "f083f2b43db573d149eaa201602a6fa8",
+    "url": "statics/icons/icon-256x256.png"
+  },
+  {
+    "revision": "f083f2b43db573d149eaa201602a6fa8",
+    "url": "statics/icons/icon-384x384.png"
+  },
+  {
+    "revision": "7da79d025be28d28588f8bdc23c1456e",
+    "url": "statics/icons/icon-512x512.png"
+  },
+  {
+    "revision": "6624870bb64b1a562e8438e57cdc636a",
+    "url": "statics/icons/ms-icon-144x144.png"
+  },
+  {
+    "revision": "ba58d8b825312a31114fb91484dadbeb",
+    "url": "statics/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "7649fba1cfc561e8aec5403bb3af670f",
+    "url": "statics/lottie/16-ok.json"
+  },
+  {
+    "revision": "80e28ec991098ef1e30fa3323f8e4570",
+    "url": "statics/lottie/17-ok.json"
+  },
+  {
+    "revision": "a08eccb79696806ac584b80c4e0a621c",
+    "url": "statics/lottie/18-ok.json"
+  },
+  {
+    "revision": "f85974cc52903cff4ad3d3e7070c074c",
+    "url": "statics/lottie/love-card.json"
+  }
+]);
